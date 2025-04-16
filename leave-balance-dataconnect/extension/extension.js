32073e023cc944ffb0ceeec81ddb3b0ec0813289
atlasmac <mckinleyatlas@gmail.com>
2025-04-16 11:19:45 -0600

    extension.js change in leave balance dc

--- leave-balance-dataconnect/extension/extension.js
+++ leave-balance-dataconnect/extension/extension.js
@@ -1,28 +1,34 @@
 module.exports = {
-    name: 'leave-balance-dataconnect',
-    publisher: '',
-    cards: [{
-        type: 'LeaveBalanceDataConnectCard',
-        source: './src/cards/LeaveBalance.jsx',
-        title: 'Leave Balance - DC',
-        displayCardType: 'Leave Balance - Data Connect',
-        description: 'Leave Balance - Data Connect',
-        configuration: {
-            client: [{
-                key: 'pipelineApi',
-                label: 'Pipeline API',
-                type: 'text',
-                required: true
-            }],
-            server: [{
-                key: 'ethosApiKey',
-                label: 'Ethos API Key',
-                type: 'password',
-                require: true
-            }]
-        }
-    }],
-    page: {
-        source: './src/page'
-    }
-}
+  name: "leave-balance-dataconnect",
+  publisher: "Atlas McKinley",
+  cards: [
+    {
+      type: "LeaveBalanceDataConnectCard",
+      source: "./src/cards/LeaveBalance.jsx",
+      title: "Leave Balance - DC",
+      displayCardType: "Leave Balance - Data Connect",
+      description: "Leave Balance - Data Connect",
+      configuration: {
+        client: [
+          {
+            key: "pipelineApi",
+            label: "Pipeline API",
+            type: "text",
+            required: true,
+          },
+        ],
+        server: [
+          {
+            key: "ethosApiKey",
+            label: "Ethos API Key",
+            type: "password",
+            require: true,
+          },
+        ],
+      },
+    },
+  ],
+  page: {
+    source: "./src/page",
+  },
+};
